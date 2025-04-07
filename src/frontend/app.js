b5052c17d736b1acb23e6f88940ae903c024f656
M4ZENN <mazen.alsawaf@studentfr.ch>
2025-04-07 11:34:07 +0200

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://ca-chy-300233-239-mazenalsawaf.wonderfulsmoke-977fbfb8.westus2.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
